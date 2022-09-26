00220bd7fdc47564a2b2aba367df439640cb6c77
franorjales <franorjales@gmail.com>
2022-09-26 19:45:23 +0200

    unused imports cleaned

--- src/main/java/chusco/portal/PortalApplication.java
+++ src/main/java/chusco/portal/PortalApplication.java
@@ -7,13 +7,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
 
 @SpringBootApplication(scanBasePackages = {"chusco"})
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
--- src/main/java/chusco/portal/PortalConfiguration.java
+++ src/main/java/chusco/portal/PortalConfiguration.java
@@ -7,10 +7,6 @@ import chusco.interfaces.IGameRepository;
 import chusco.interfaces.IGameServices;
 import chusco.repository.GameRepositoryImpl;
 import chusco.services.GameServicesImpl;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class PortalConfiguration {
